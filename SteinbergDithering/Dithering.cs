ac597f7fd300ea777d14dfe5fb29c2dfc39d7f6e
Göksel Küçükşahin <gokselkucuksahin@gmail.com>
2019-09-25 23:19:54 +0300

    Add files via upload

---
+++ SteinbergDithering/Dithering.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SteinbergDithering
+{
+    class Dithering
+    {
+        private static Bitmap image; //main image
+        private static Bitmap bmp; //target image
+
+        private static float errR; //error of Red
+        private static float errG; //error of Green
+        private static float errB; //error of Blue
+
+        public static Bitmap Make(Bitmap original, int factor)
+        {
+            SetImage(original);
+            return Dither(image, factor);
+        }
+
+        private static void SetImage(Bitmap img)
+        {
+            if (img != null)
+            {
+                //if not null
+                image = img;
+                bmp = (Bitmap)image.Clone();
+            }
+            else
+            {
+                //if null throw exception.
+                throw new NullReferenceException();
+            }
+        }
+
+        private static Bitmap GetImage()
+        {
+            return bmp;
+        }
+
+        private static Bitmap Dither(Bitmap ditherImage, int factor)
+        {
+            for (int y = 0; y < ditherImage.Height - 1; y++)
+            {
+                for (int x = 1; x < ditherImage.Width - 1; x++)
+                {
+                    Color pix = ditherImage.GetPixel(x, y);
+                    float oldR = pix.R;
+                    float oldG = pix.G;
+                    float oldB = pix.B;
+
+                    int newR = (int)Math.Round(factor * oldR / 255) * (255 / factor);
+                    int newG = (int)Math.Round(factor * oldG / 255) * (255 / factor);
+                    int newB = (int)Math.Round(factor * oldB / 255) * (255 / factor);
+
+                    ditherImage.SetPixel(x, y, Color.FromArgb(newR, newG, newB));
+
+                    errR = oldR - newR;
+                    errG = oldG - newG;
+                    errB = oldB - newB;
+
+                    SetPx(ditherImage, x + 1, y, 7);
+
+                    SetPx(ditherImage, x - 1, y + 1, 3);
+
+                    SetPx(ditherImage, x, y + 1, 5);
+
+                    SetPx(ditherImage, x + 1, y + 1, 1);
+                }
+            }
+            return ditherImage;
+        }
+
+        private static void SetPx(Bitmap bmap, int x, int y, int factor)
+        {
+            //Color c = kitten.GetPixel(x + 1, y);
+            Color c = bmap.GetPixel(x, y);
+            float r = c.R;
+            float g = c.G;
+            float b = c.B;
+
+            r = r + (float)(errR * (factor / 16.0));
+            g = g + (float)(errG * (factor / 16.0));
+            b = b + (float)(errB * (factor / 16.0));
+            if (r < 0)
+            {
+                r = 0;
+            }
+            else if (r > 255)
+            {
+                r = 255;
+            }
+            if (g < 0)
+            {
+                g = 0;
+            }
+            else if (g > 255)
+            {
+                g = 255;
+            }
+            if (b < 0)
+            {
+                b = 0;
+            }
+            else if (b > 255)
+            {
+                b = 255;
+            }
+            c = Color.FromArgb((int)r, (int)g, (int)b);
+            bmap.SetPixel(x, y, c);
+        }
+
+    }
+}
---
+++ SteinbergDithering/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace SteinbergDithering
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxView = new System.Windows.Forms.PictureBox();
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.buttonDither = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxView
+            // 
+            this.pictureBoxView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBoxView.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBoxView.Location = new System.Drawing.Point(12, 12);
+            this.pictureBoxView.Name = "pictureBoxView";
+            this.pictureBoxView.Size = new System.Drawing.Size(604, 476);
+            this.pictureBoxView.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxView.TabIndex = 0;
+            this.pictureBoxView.TabStop = false;
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonLoad.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonLoad.Location = new System.Drawing.Point(168, 494);
+            this.buttonLoad.MaximumSize = new System.Drawing.Size(94, 37);
+            this.buttonLoad.MinimumSize = new System.Drawing.Size(94, 37);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(94, 37);
+            this.buttonLoad.TabIndex = 1;
+            this.buttonLoad.Text = "Load";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            this.buttonLoad.Click += new System.EventHandler(this.buttonLoad_Click);
+            // 
+            // buttonDither
+            // 
+            this.buttonDither.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonDither.Enabled = false;
+            this.buttonDither.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonDither.Location = new System.Drawing.Point(268, 494);
+            this.buttonDither.MaximumSize = new System.Drawing.Size(94, 37);
+            this.buttonDither.MinimumSize = new System.Drawing.Size(94, 37);
+            this.buttonDither.Name = "buttonDither";
+            this.buttonDither.Size = new System.Drawing.Size(94, 37);
+            this.buttonDither.TabIndex = 2;
+            this.buttonDither.Text = "Dither";
+            this.buttonDither.UseVisualStyleBackColor = true;
+            this.buttonDither.Click += new System.EventHandler(this.buttonDither_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSave.Enabled = false;
+            this.buttonSave.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonSave.Location = new System.Drawing.Point(368, 494);
+            this.buttonSave.MaximumSize = new System.Drawing.Size(94, 37);
+            this.buttonSave.MinimumSize = new System.Drawing.Size(94, 37);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(94, 37);
+            this.buttonSave.TabIndex = 3;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(628, 543);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonDither);
+            this.Controls.Add(this.buttonLoad);
+            this.Controls.Add(this.pictureBoxView);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(800, 600);
+            this.MinimumSize = new System.Drawing.Size(644, 582);
+            this.Name = "Form1";
+            this.Text = "JellyBeanci Steinberg Dithering";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxView;
+        private System.Windows.Forms.Button buttonLoad;
+        private System.Windows.Forms.Button buttonDither;
+        private System.Windows.Forms.Button buttonSave;
+    }
+}
+
